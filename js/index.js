591756c2d45fde0cd6e29ae6ed7736f069cc26f6
Mykhailo Yaremchuk <mikhailya4@gmail.com>
2023-12-08 18:01:34 +0200

    Try 2

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
+// - Створення елементів через клас
+// - Видалення завдань поштучно
+// - Очищення всього переліку завдань
+// - Виконанні завдання приховувати з можливість переглядати їх окремо
+// - зберігання завдань в кеші
+// - зберігання завдань в базі даних
+
+
 class Task  {
     // constructor(title, discription, date, time) {
     //     this._title = title;
     //     this._discription = discription;
     //     this._date = date;
     //     this._time = time;
-
     // }
     constructor(title) {
         this._title = title;
@@ -23,17 +30,22 @@ const taskCreatorInput = document.querySelector('.task-creator__input');
 const taskCreatorButton = document.querySelector('.task-creator__button');
 
 const createTask = function() {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("task-child");
-    newDiv.style.display = "flex";
-    taskCreated.prepend(newDiv);
-    newDiv.innerHTML = `
-        <div class="task-child__checkboks"><input type="checkbox"></div>
-        <div class="task-child__created-task">${taskCreatorInput.value}</div>
-    `;
+    const emptyRegExp = /^(\s+)/;
+    if (taskCreatorInput.value !== "" && !emptyRegExp.test(taskCreatorInput.value)) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("task-child");
+        newDiv.style.display = "flex";
+        taskCreated.prepend(newDiv);
+        newDiv.innerHTML = `
+            <div class="task-child__checkboks"><input type="checkbox"></div>
+            <div class="task-child__created-task">${taskCreatorInput.value}</div>
+        `;
+    }
     taskCreatorInput.value = "";
+    taskCreatorInput.focus();
 }
 
 taskCreatorButton.addEventListener('click', createTask);
-
-
+taskCreatorInput.addEventListener('keyup', (e) => {
+    if (e.key === "Enter") createTask();
+});
